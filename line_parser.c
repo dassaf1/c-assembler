60924488d1dfa19e1227b097b62f8110d8f06dbe
OpenU student <student@ubuntu.(none)>
2017-08-05 12:16:44 -0400

    add line_parser.c

---
+++ line_parser.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "data_structures.h"
+
+sentence * parse_sentence(char *line, int line_number) {
+	
+	sentence *parsed_sentence = (sentence*)malloc(sizeof(sentence));
+
+
+	return parsed_sentence; 
+
+}
