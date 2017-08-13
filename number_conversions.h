010686a4d842e808629c17207617ebccaeb3595f
OpenU student <student@ubuntu.(none)>
2017-08-13 15:05:26 -0400

    fixing number_conversions.h

--- number_conversions.h
+++ number_conversions.h
@@ -1,8 +1,13 @@
-char * convert_binary_to_base_4_strange(char binary_address[10]);
+#ifndef _NUMBER_CONVERSIONS_H_
+#define _NUMBER_CONVERSIONS_H_
+
+/* char * convert_binary_to_base_4_strange(char binary_address[10]); */
 
 char * convert_base_4_to_base_4_strange(int num); /* DONE */
-int convert_dec_to_another_base(int num); /* DONE */
+int convert_dec_to_another_base(int num, int base); 
 
-char * convert_ascii_value_to_binary(char c); /* NEEDED (10 bits - Ripud afasim + string termination) */
+void convert_ascii_value_to_10_bit_binary(char c, char * arr); /* DONE */
 void convert_dec_to_x_bit_binary(int num, int bits, char * arr); /* DONE */
 
+
+#endif
