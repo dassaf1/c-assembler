b21bab40a0e21763d1bdfc60d97181e7c7a591ae
Dafna Assaf <dafna.assaf@gmail.com>
2017-08-17 01:10:05 +0300

    Completing number_conversions header file

--- number_conversions.h
+++ number_conversions.h
@@ -1,5 +1,3 @@
-#ifndef _NUMBER_CONVERSIONS_H_
-#define _NUMBER_CONVERSIONS_H_
 
 /* char * convert_binary_to_base_4_strange(char binary_address[10]); */
 
@@ -9,5 +7,10 @@ int convert_dec_to_another_base(int num, int base);
 void convert_ascii_value_to_10_bit_binary(char c, char * arr); /* DONE */
 void convert_dec_to_x_bit_binary(int num, int bits, char * arr); /* DONE */
 
+void convert_binary_to_base_4_strange(char binary_address[11], char * result);
+void convert_base_4_to_base_4_strange(int num, char * result);
+int convert_dec_to_another_base(int num, int base);
+void convert_dec_to_base_4_strange(int num, char * result);
+void convert_ascii_value_to_10_bit_binary(char c, char * result);
+void convert_dec_to_x_bit_binary(int num, int bits, char * result);
 
-#endif
