c75f7c03ea037bd24ac6e17d8ff0d50f23ccbf39
Dafna Assaf <dafna.assaf@gmail.com>
2017-08-17 01:13:10 +0300

    fixed number conversions header file

--- number_conversions.h
+++ number_conversions.h
@@ -1,11 +1,5 @@
-
-/* char * convert_binary_to_base_4_strange(char binary_address[10]); */
-
-char * convert_base_4_to_base_4_strange(int num); /* DONE */
-int convert_dec_to_another_base(int num, int base); 
-
-void convert_ascii_value_to_10_bit_binary(char c, char * arr); /* DONE */
-void convert_dec_to_x_bit_binary(int num, int bits, char * arr); /* DONE */
+#ifndef _NUMBER_CONVERSIONS_H_ 
+#define _NUMBER_CONVERSIONS_H_
 
 void convert_binary_to_base_4_strange(char binary_address[11], char * result);
 void convert_base_4_to_base_4_strange(int num, char * result);
@@ -14,3 +8,4 @@ void convert_dec_to_base_4_strange(int num, char * result);
 void convert_ascii_value_to_10_bit_binary(char c, char * result);
 void convert_dec_to_x_bit_binary(int num, int bits, char * result);
 
+#endif 
