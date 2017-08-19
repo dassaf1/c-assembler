f037dd200cf485e1fdc7aeb654e394925e18047b
OpenU student <student@ubuntu.(none)>
2017-08-19 09:08:57 -0400

    line_parser.h

--- line_parser.h
+++ line_parser.h
@@ -1,7 +1,8 @@
 #ifndef _PARSER_H_
 #define _PARSER_H_
 
-sentence* parse_sentence(char*, int, int*);
+sentence* parse_sentence(char*, int, int*); 
+int strcmp_lower(char *word1, char *word2);
 
 
 #endif
